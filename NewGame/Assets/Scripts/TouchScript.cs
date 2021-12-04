007f83a31087f17f269f4467c9ff995cc7574e2d
hypevade <schkalka@gmail.com>
2021-12-05 00:48:32 +0400

    AddButtons(Beta)(их не трогать)

---
+++ NewGame/Assets/Scripts/TouchScript.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour
+{
+
+    public void OnMouseDown()
+    {
+        print("ok");
+    }
+}
