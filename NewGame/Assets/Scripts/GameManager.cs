25b7ee8da60d7cab36d7e9895e67b494d4d7984d
Dspsyuodd <78037644+Dspsyuodd@users.noreply.github.com>
2021-12-05 15:34:18 +0400

    1234

---
+++ NewGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public bool GameIsOver;
+    public bool GameIsStarted;
+    public bool GameIsPaused;
+    public float GameScore;
+    public int CoinScore;
+    public int BusterScore;
+}
