aa848bea53e76036b5cb3900a83fcf65fa9d8739
Dspsyuodd <78037644+Dspsyuodd@users.noreply.github.com>
2021-12-05 14:05:32 +0400

    123321

---
+++ NewGame/Assets/Scripts/DestroyTerrain.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTerrain : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 nextPos;
+    [SerializeField]
+    private GameObject _roadPrefab;
+
+    private readonly Vector3 uberCostil = new Vector3(-3.67362f, 0f, 3.64247f);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        if(other.tag == "Road")
+            GenerateNext();
+    }
+
+    void Start()
+    {
+        GenerateNext();
+        GenerateNext();
+        GenerateNext();
+        GenerateNext();
+    }
+
+    public void GenerateNext()
+    {
+        var flag = Random.Range(0, 2);
+
+        var temp = Instantiate(_roadPrefab, nextPos, Quaternion.identity);
+        if (flag == 1)
+        {
+            temp.transform.GetChild(0).localScale = new Vector3(-1, 1, 1);
+            temp.transform.position += uberCostil;
+        }
+
+        nextPos = temp.transform.GetChild(0).GetChild(8).position;
+    }
+
+}
