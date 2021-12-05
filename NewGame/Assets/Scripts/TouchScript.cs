6bfc85c9dbd65c30146db22d120bfaba9b23ecb5
hypevade <schkalka@gmail.com>
2021-12-05 14:12:19 +0400

    Add GameMenu

--- NewGame/Assets/Scripts/TouchScript.cs
+++ NewGame/Assets/Scripts/TouchScript.cs
@@ -1,12 +1,57 @@
 using System;
-using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TouchScript : MonoBehaviour
 {
+    public GameObject startPanel;
+    public GameObject gamePanel;
+    public GameObject pausePanel;
+    public GameObject gameOverPanel;
 
-    public void OnMouseDown()
+    public bool gameIsStarted;
+    public bool gameIsPaused;
+    public bool gameIsOver;
+
+
+    public void StartGame()
     {
-        print("ok");
+        startPanel.SetActive(false);
+        gamePanel.SetActive(true);
+        gameIsStarted = true;
     }
+
+    public void StartPause()
+    {
+        pausePanel.SetActive(true);
+        gameIsPaused = true;
+    }
+    
+    public void EndPause()
+    {
+        pausePanel.SetActive(false);
+        gameIsPaused = false;
+    }
+
+    public void Turn(bool isRightTurn)
+    {
+        if (isRightTurn)
+        {
+            //поворот вправо
+        }
+        // поворот влево
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        gameIsOver = true;
+    }
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }
